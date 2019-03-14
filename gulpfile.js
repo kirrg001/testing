52d0ba3bba4a68176b1403e5316b6a85a8b535f6
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-14 15:54:28 +0100

    working on it

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,62 @@
+const path = require('path');
+
 exports.release = () => {
     const releaseUtils = require('@tryghost/release-utils');
-    console.log('lol');
+    const newVersion = process.env.npm_package_version;
 
-    console.log(process.env.npm_package_version);
-    return;
+    if (!newVersion || newVersion === '') {
+        console.log('Invalid version.');
+        return;
+    }
+
+    console.log(`Draft release for ${newVersion}.`);
+
+    const changelog = new releaseUtils.Changelog({
+        changelogPath: path.join(process.cwd(), '.', 'changelog.md'),
+        folder: path.join(process.cwd(), '.')
+    });
+
+    return releaseUtils
+        .releases
+        .get({
+            userAgent: 'casper',
+            uri: 'https://api.github.com/repos/kirrg001/testing/releases'
+        })
+        .then((response) => {
+            if (!response || !response.length) {
+                console.log('No releases found. Skipping');
+                return;
+            }
+
+            let previousVersion = response[0].name;
+
+            console.log(`previous version ${previousVersion}`);
+
+            changelog
+                .write({
+                    githubRepoPath: 'https://github.com/kirrg001/testing',
+                    lastVersion: previousVersion
+                })
+                .sort()
+                .clean();
+
+            releaseUtils
+                .releases
+                .create({
+                    draft: true,
+                    preRelease: false,
+                    tagName: newVersion,
+                    releaseName: newVersion,
+                    userAgent: 'casper',
+                    uri: 'https://api.github.com/repos/kirrg001/testing/releases',
+                    github: {
+                        username: configuration.github.username,
+                        token: configuration.github.token
+                    },
+                    changelogPath: path.join(process.cwd(), 'changelog.md')
+                })
+                .then((response)=> {
+                    console.log('Release draft generated: ' + response.releaseUrl);
+                });
+        });
 };
