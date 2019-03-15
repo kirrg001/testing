cd7b499008dfd4dae9a410dae072461b214e5c5c
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-15 14:08:12 +0100

    lul

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ exports.release = () => {
     // https://yarnpkg.com/lang/en/docs/cli/version/
     const newVersion = process.env.npm_package_version;
 
-    console.log(argv);
+    console.log(process.argv);
 
     if (!newVersion || newVersion === '') {
         console.log('Invalid version.');
